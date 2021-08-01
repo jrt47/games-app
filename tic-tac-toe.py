09b5e91ab630cf6bd81924bd410bbfdcb4da6877
jrt47 <jrt47@students.cs.ubc.ca>
2021-07-31 20:55:27 -0700

    Added reset functionality and improved GUI for tic tac toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,15 @@
 from tkinter import *
 
+WIDTH = 234
+HEIGHT = 259
+
 BUTTON_HEIGHT = 2
 BUTTON_WIDTH = 4
+
+X_PADDING = 60
+Y_PADDING = 10
+
+TITLE = "Tic Tac Toe"
 
 board = []
 
@@ -17,6 +25,7 @@ def reset():
     turn_counter = 1
     label_text.set("It's X's turn.")
     update_button_text()
+    enable_buttons()
 
 
 def button_click(position):
@@ -27,18 +36,31 @@ def button_click(position):
         update_button_text()
         if check_for_winner():
             label_text.set(f"{player}'s won!")
+            disable_buttons()
             return
         if check_for_tie():
             label_text.set("It's a tie.")
+            disable_buttons()
             return
         turn_counter += 1
         player = get_current_player()
         label_text.set(f"It's {player}'s turn.")
 
 
+def disable_buttons():
+    for i in range(9):
+        buttons[i]["state"] = "disabled"
+
+
+def enable_buttons():
+    for i in range(9):
+        buttons[i]["state"] = "normal"
+
+
 def update_button_text():
     for i in range(9):
         button_texts[i].set(board[i])
+
 
 def get_current_player():
     if turn_counter % 2 == 1:
@@ -76,7 +98,10 @@ def check_for_tie():
 
 
 root = Tk()
-root.title("Tic Tac Toe")
+root.title(TITLE)
+
+title = Label(root, text=TITLE, font=("TkDefaultFont",15))
+title.grid(row=0, column=0, columnspan=3, pady=(Y_PADDING, 0))
 
 button_text_0 = StringVar()
 button_text_1 = StringVar()
@@ -93,38 +118,60 @@ button_texts = [button_text_0, button_text_1, button_text_2,
                 button_text_6, button_text_7, button_text_8]
 
 button_0 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_0, command=lambda: button_click(0))
+                  textvariable=button_text_0, command=lambda:button_click(0))
 button_1 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_1, command=lambda: button_click(1))
+                  textvariable=button_text_1, command=lambda:button_click(1))
 button_2 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_2, command=lambda: button_click(2))
+                  textvariable=button_text_2, command=lambda:button_click(2))
 button_3 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_3, command=lambda: button_click(3))
+                  textvariable=button_text_3, command=lambda:button_click(3))
 button_4 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_4, command=lambda: button_click(4))
+                  textvariable=button_text_4, command=lambda:button_click(4))
 button_5 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_5, command=lambda: button_click(5))
+                  textvariable=button_text_5, command=lambda:button_click(5))
 button_6 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_6, command=lambda: button_click(6))
+                  textvariable=button_text_6, command=lambda:button_click(6))
 button_7 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_7, command=lambda: button_click(7))
+                  textvariable=button_text_7, command=lambda:button_click(7))
 button_8 = Button(root, height=BUTTON_HEIGHT, width=BUTTON_WIDTH,
-                  textvariable=button_text_8, command=lambda: button_click(8))
+                  textvariable=button_text_8, command=lambda:button_click(8))
 
-button_0.grid(row=0, column=0)
-button_1.grid(row=0, column=1)
-button_2.grid(row=0, column=2)
-button_3.grid(row=1, column=0)
-button_4.grid(row=1, column=1)
-button_5.grid(row=1, column=2)
-button_6.grid(row=2, column=0)
-button_7.grid(row=2, column=1)
-button_8.grid(row=2, column=2)
+buttons = [button_0, button_1, button_2,
+           button_3, button_4, button_5,
+           button_6, button_7, button_8]
+
+button_0.grid(row=1, column=0, padx=(X_PADDING, 0), pady=(Y_PADDING, 0))
+button_1.grid(row=1, column=1, pady=(Y_PADDING,0))
+button_2.grid(row=1, column=2, padx=(0, X_PADDING), pady=(Y_PADDING, 0))
+button_3.grid(row=2, column=0, padx=(X_PADDING, 0))
+button_4.grid(row=2, column=1)
+button_5.grid(row=2, column=2, padx=(0, X_PADDING))
+button_6.grid(row=3, column=0, padx=(X_PADDING, 0), pady=(0, Y_PADDING))
+button_7.grid(row=3, column=1, pady=(0,Y_PADDING))
+button_8.grid(row=3, column=2, padx=(0, X_PADDING), pady=(0, Y_PADDING))
 
 label_text = StringVar()
 label = Label(root, textvariable=label_text)
-label.grid(row=3, column=0, columnspan=3)
+label.grid(row=4, column=0, columnspan=3)
+
+reset_button = Button(root, text="Reset", command=reset)
+reset_button.grid(row=5, column=0, columnspan=3, pady=(10,20))
 
 
-reset()
-root.mainloop()
+def place_window():
+    w = WIDTH
+    h = HEIGHT
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = ws/2 - w/2
+    y = hs/2 - h/2
+    root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
+
+def start():
+    reset()
+    place_window()
+    root.mainloop()
+
+
+start()
